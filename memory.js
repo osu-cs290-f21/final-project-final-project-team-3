f88bba8f560d5a295aeec024db8f89defc0de64e
Steven Vu <MY_NAME@example.com>
2021-11-29 20:06:10 -0800

    Started js file for memory html

---
+++ memory.js
@@ -0,0 +1,7 @@
+var reactionStartButton = document.getElementById("reaction-start-button");
+
+reactionStartButton.addEventListener("click", started);
+
+function started (event) {
+    console.log("You pressed the start button!");
+}
