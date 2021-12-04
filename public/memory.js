5ef36f1f7b33cbd5e95183f2ed58e35c18fe191b
Isaiah Wachsmuth <wachsmui@oregonstate.edu>
2021-12-03 18:10:39 -0800

    Starting the memory file

---
+++ public/memory.js
@@ -0,0 +1,13 @@
+var memoryButton = document.getElementsByClassName('memory-start-button')
+
+var words = { lake, appointment, sense, leash, silk, assume, worth, consensus,
+  incapable, engine, stumble, blast, combination, drink, guide, craft, separation,
+  harmony, heat, verdict, reactor, manual, hero, railroad, jest, influence, tube,
+  wonder, lip, tabloid
+}
+
+
+
+memoryButton.addEventListerner('click', function () {
+  console.log(words[4]);
+})
